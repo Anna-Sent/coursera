d694140ce0c2db53af95be1b57ebe618e24f90bd
Anna Sentyakova <anna.sent@gmail.com>
2019-05-14 14:09:21 +0400

    Mastermind: it syntax

--- Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -7,11 +7,11 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
 
-    val rightPositions = secret.zip(guess).count { pair -> pair.first == pair.second }
+    val rightPositions = secret.zip(guess).count { it.first == it.second }
 
     val commonLetters = "ABCDEF".sumBy { ch ->
 
-        Math.min(secret.count { ch1 -> ch1 == ch }, guess.count { ch1 -> ch1 == ch })
+        Math.min(secret.count { it == ch }, guess.count { it == ch })
     }
     return Evaluation(rightPositions, commonLetters - rightPositions)
 }
