389bf1bbeac52a4fd1b5b6c46afd8ba25c730d88
Anna Sentyakova <anna.sent@gmail.com>
2019-05-15 18:23:42 +0400

    Rationals: started

--- Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
+++ Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
@@ -1,6 +1,60 @@
 package rationals
 
+import java.math.BigInteger
 
+data class Rational(val n: BigInteger, val d: BigInteger) : Comparable<Rational> {
+
+    constructor(n: Int, d: Int) : this(n.toBigInteger(), d.toBigInteger())
+
+    constructor(n: Long, d: Long) : this(n.toBigInteger(), d.toBigInteger())
+
+    init {
+        if (d == BigInteger.ZERO) {
+            throw IllegalArgumentException("Denominator can't be zero")
+        }
+    }
+
+    fun normalize(): Rational {
+        val divisor = n.gcd(d)
+        val nSign = if (n >= BigInteger.ZERO) BigInteger.ONE else BigInteger.valueOf(-1)
+        val dSign = if (d >= BigInteger.ZERO) BigInteger.ONE else BigInteger.valueOf(-1)
+        val sign = nSign * dSign
+        return Rational(sign * (n / divisor).abs(), (d / divisor).abs())
+    }
+
+    override fun compareTo(other: Rational): Int {
+        TODO()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Rational) return false
+        val normalizedThis = normalize()
+        val normalizedOther = other.normalize()
+        return normalizedThis.n == normalizedOther.n
+                && normalizedThis.d == normalizedOther.d
+    }
+
+    override fun hashCode(): Int {
+        val prime = 31
+        var result = 1
+        result = prime * result + n.hashCode()
+        result = prime * result + d.hashCode()
+        return result
+    }
+}
+
+infix fun Int.divBy(d: Int): Rational = Rational(this, d)
+infix fun Long.divBy(d: Long): Rational = Rational(this, d)
+infix fun BigInteger.divBy(d: BigInteger): Rational = Rational(this, d)
+
+operator fun Rational.plus(other: Rational): Rational = TODO()
+operator fun Rational.minus(other: Rational): Rational = TODO()
+operator fun Rational.unaryMinus(): Rational = TODO()
+operator fun Rational.div(other: Rational): Rational = TODO()
+operator fun Rational.times(other: Rational): Rational = TODO()
+
+fun String.toRational(): Rational = TODO()
 
 fun main() {
     val half = 1 divBy 2
