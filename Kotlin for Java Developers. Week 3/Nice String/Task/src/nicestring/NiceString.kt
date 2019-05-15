fa4e431ed89b5ce3227be119857e642760a5ef37
Anna Sentyakova <anna.sent@gmail.com>
2019-05-15 13:42:01 +0400

    Nice String, Taxi Park: refactoring

--- Kotlin for Java Developers. Week 3/Nice String/Task/src/nicestring/NiceString.kt
+++ Kotlin for Java Developers. Week 3/Nice String/Task/src/nicestring/NiceString.kt
@@ -1,23 +1,19 @@
 package nicestring
 
-fun String.isNice(): Boolean {
-    return listOf(
-            first(),
-            second(),
-            third())
-            .count { it } >= 2
-}
+fun String.isNice(): Boolean =
+        listOf(
+                first(),
+                second(),
+                third())
+                .count { it } >= 2
 
-fun String.first(): Boolean {
-    return setOf("bu", "ba", "be")
-            .none { contains(it) }
-}
+fun String.first(): Boolean =
+        setOf("bu", "ba", "be")
+                .none { contains(it) }
 
-fun String.second(): Boolean {
-    return count { it in setOf('a', 'e', 'i', 'o', 'u') } >= 3
-}
+fun String.second(): Boolean =
+        count { it in setOf('a', 'e', 'i', 'o', 'u') } >= 3
 
-fun String.third(): Boolean {
-    return zipWithNext()
-            .any { it.first == it.second }
-}
+fun String.third(): Boolean =
+        zipWithNext()
+                .any { it.first == it.second }
--- Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -5,87 +5,71 @@ package taxipark
  */
 fun TaxiPark.findFakeDrivers(): Set<Driver> =
         allDrivers.associate { driver -> driver to findAllTrips(driver) }
-                .filter { (_, trips) -> trips.isEmpty() }
-                .map { (driver, _) -> driver }
-                .toSet()
+                .filterValues { trips -> trips.isEmpty() }
+                .keys
 
-fun TaxiPark.findAllTrips(driver: Driver): List<Trip> {
-    return trips.filter { trip -> trip.driver == driver }
-}
+fun TaxiPark.findAllTrips(driver: Driver): List<Trip> =
+        trips.filter { trip -> trip.driver == driver }
 
 /*
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
 fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
         allPassengers.associate { passenger -> passenger to findAllTrips(passenger) }
-                .filter { (_, trips) -> trips.size >= minTrips }
-                .map { (passenger, _) -> passenger }
-                .toSet()
+                .filterValues { trips -> trips.size >= minTrips }
+                .keys
 
-fun TaxiPark.findAllTrips(passenger: Passenger): List<Trip> {
-    return trips.filter { trip -> passenger in trip.passengers }
-}
+fun TaxiPark.findAllTrips(passenger: Passenger): List<Trip> =
+        trips.filter { trip -> passenger in trip.passengers }
 
 /*
  * Task #3. Find all the passengers, who were taken by a given driver more than once.
  */
 fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> =
         allPassengers.associate { passenger -> passenger to findAllTrips(passenger) }
-                .filter { (_, trips) -> trips.count { trip -> trip.driver == driver } > 1 }
-                .map { (passenger, _) -> passenger }
-                .toSet()
+                .filterValues { trips -> trips.count { trip -> trip.driver == driver } > 1 }
+                .keys
 
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
  */
 fun TaxiPark.findSmartPassengers(): Set<Passenger> =
         allPassengers.associate { passenger -> passenger to findAllTrips(passenger) }
-                .filter { (_, trips) ->
+                .filterValues { trips ->
                     val (tripsWithDiscount, tripsNoDiscount) = trips.partition { trip -> trip.discount != null }
                     tripsWithDiscount.size > tripsNoDiscount.size
                 }
-                .map { (passenger, _) -> passenger }
-                .toSet()
+                .keys
 
 /*
  * Task #5. Find the most frequent trip duration among minute periods 0..9, 10..19, 20..29, and so on.
  * Return any period if many are the most frequent, return `null` if there're no trips.
  */
-fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
-    if (trips.isEmpty()) return null
-    val durations = trips.map { trip -> trip.duration }
-            .sortedBy { it }
-    val min = durations.min()!! / 10 * 10
-    val max = durations.max()!! / 10 * 10
-    return generateSequence(min) { it + 10 }
-            .takeWhile { it <= max }
-            .associate { index -> IntRange(index, index + 9) to durations.filter { duration -> duration in index..index + 9 } }
-            .toList()
-            .sortedBy { (_, durations) -> durations.size }
-            .map { (range, _) -> range }
-            .last()
-}
+fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? =
+        trips.groupBy { trip ->
+            val start = trip.duration / 10 * 10
+            val end = start + 9
+            start..end
+        }
+                .maxBy { (_, trips) -> trips.size }
+                ?.key
 
 /*
  * Task #6.
  * Check whether 20% of the drivers contribute 80% of the income.
  */
-fun TaxiPark.checkParetoPrinciple(): Boolean {
-    return trips.isNotEmpty() && findIncomeOf20PercentDrivers() / findTotalIncome() >= 0.8
-}
+fun TaxiPark.checkParetoPrinciple(): Boolean =
+        trips.isNotEmpty() && findIncomeOf20PercentDrivers() / findTotalIncome() >= 0.8
 
-fun TaxiPark.findIncomeOf20PercentDrivers(): Double {
-    return allDrivers.map { driver -> findIncome(driver) }
-            .sortedByDescending { it }
-            .take((allDrivers.size * 0.2).toInt())
-            .sumByDouble { it }
-}
+fun TaxiPark.findIncomeOf20PercentDrivers(): Double =
+        allDrivers.map { driver -> findIncome(driver) }
+                .sortedByDescending { it }
+                .take((allDrivers.size * 0.2).toInt())
+                .sumByDouble { it }
 
-fun TaxiPark.findIncome(driver: Driver): Double {
-    return trips.filter { trip -> trip.driver == driver }
-            .sumByDouble { trip -> trip.cost }
-}
+fun TaxiPark.findIncome(driver: Driver): Double =
+        trips.filter { trip -> trip.driver == driver }
+                .sumByDouble { trip -> trip.cost }
 
-fun TaxiPark.findTotalIncome(): Double {
-    return trips.sumByDouble { trip -> trip.cost }
-}
+fun TaxiPark.findTotalIncome(): Double =
+        trips.sumByDouble { trip -> trip.cost }
