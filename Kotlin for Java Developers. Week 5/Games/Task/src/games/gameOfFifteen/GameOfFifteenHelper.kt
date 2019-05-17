f2437928ddc340e3a522cd93f223a5d90c3f1bb1
Anna Sentyakova <anna.sent@gmail.com>
2019-05-18 01:11:50 +0400

    Game: isEven

--- Kotlin for Java Developers. Week 5/Games/Task/src/games/gameOfFifteen/GameOfFifteenHelper.kt
+++ Kotlin for Java Developers. Week 5/Games/Task/src/games/gameOfFifteen/GameOfFifteenHelper.kt
@@ -11,5 +11,37 @@ package games.gameOfFifteen
  * Thus the initial permutation should be correct.
  */
 fun isEven(permutation: List<Int>): Boolean {
-    TODO()
+    val list = ArrayList(permutation)
+    val (_, count) = sort(list)
+    return count % 2 == 0
+}
+
+fun sort(list: List<Int>): Pair<List<Int>, Int> {
+    val sorted = ArrayList(list)
+    val size = sorted.size
+    var count = 0
+    for (i in 0 until size) {
+        for (j in 0 until size - i - 1) {
+            if (sorted[j] > sorted[j + 1]) {
+                val tmp = sorted[j]
+                sorted[j] = sorted[j + 1]
+                sorted[j + 1] = tmp
+                ++count
+            }
+        }
+    }
+    return sorted to count
+}
+
+fun main() {
+    test(listOf(1, 2, 3))
+    test(listOf())
+    test(listOf(3, 2, 1))
+    test(listOf(-1, -2, -3))
+    test(listOf(-1, -2, -3, 0, 1, 2, 3))
+}
+
+fun test(list: List<Int>) {
+    val (sorted, _) = sort(list)
+    println(sorted.toString() == list.sorted().toString())
 }
